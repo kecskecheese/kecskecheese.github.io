23a3fa93da2d000d6a9e28b4976fbea66287c399
kecskecheese <96261163+kecskecheese@users.noreply.github.com>
2022-06-09 16:18:51 +0200

    Add files via upload

---
+++ homepage/README.md
@@ -0,0 +1,3 @@
+# homepage
+homepage to use in your browser
+<p>this is a copy of <a href="https://github.com/Jaredk3nt/homepage">Jaredk3nt's homepage</a> recolored into <a href="https://github.com/catppuccin/catppuccin"> catpuccin </a> colors.
---
+++ homepage/bookmarks.js
@@ -0,0 +1,38 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Gmail", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "GDrive", url: "https://drive.google.com" },
+      { name: "MozaNaplo", url: "https://lonyay.mozanaplo.hu" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "Main", url: "https://reddit.com/" },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      { name: "/r/ArchLinux", url: "https://reddit.com/r/archlinux" },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Messenger", url: "https://messenger.com" },
+    ],
+  },
+];
